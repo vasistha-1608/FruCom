e16da50115d50e0fe3b8e01298ea3e436cbabcb3
vasistha-1608 <erankivasistha@gmail.com>
2026-01-27 20:43:54 -0600

    refactor:Migrate to C++ standalone skeleton v0.0.1

---
+++ apps/chat.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+
+int main(){
+    std :: cout << "FruCom Engine : Skeleton Build successful !! Congratulations on version v0.0.1" << std :: endl;
+    return 0;
+}
--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <torch/extension.h>
-
-void launch_add(torch::Tensor x, torch::Tensor y, torch::Tensor out);
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("add", &launch_add, "CUDA Vector Add");
-}
